ab26f3ac975fcea747af100c28acdfd57f06946c
ThiagoThalisson <atomic-feast@proton.me>
2025-03-02 11:56:11 -0300

    Centralizes data in a data folder

---
+++ src/data/siteData.ts
@@ -0,0 +1,15 @@
+export const heroData = {
+  bgImage: "/Hero/Hero.webp",
+  logoSrc: "/Logo.svg",
+  logoAlt: "Um fundo em vermelho vinho com um sol no centro",
+  title: "O Amor Está Doente",
+  firstDescription:
+    "Ajude-se a romper com padrões disfuncionais e restaurar sua capacidade de amar de forma saudável e autêntica.",
+  secondDescription:
+    "O amor, que deveria nutrir, tem se tornado um campo de frustrações e dores. Neste e-book, você descobrirá as raízes desse adoecimento emocional e aprenderá técnicas de cura para transformar suas relações e viver o amor em sua plenitude.",
+  videoSrc: "https://www.youtube.com/embed/uk4wPrid9io?si=LKWNlEtAL1yZnQ7l",
+  buttonText: "Baixe e transforme sua forma de amar!",
+  buttonLink: "",
+};
+
+export default heroData;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,22 +2,13 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import { Hero } from "../components/hero/hero";
+import heroData from "../data/siteData";
 
 export default component$(() => {
   return (
     <>
       <p>Happy coding.</p>
-      <Hero
-        bgImage="/Hero/Hero.webp"
-        logoSrc="/Logo.svg"
-        logoAlt="Um fundo em vermelho vinho com um sol no centro"
-        title="O Amor Está Doente"
-        firstDescription="Ajude-se a romper com padrões disfuncionais e restaurar sua capacidade de amar de forma saudável e autêntica."
-        secondDescription="O amor, que deveria nutrir, tem se tornado um campo de frustrações e dores. Neste e-book, você descobrirá as raízes desse adoecimento emocional e aprenderá técnicas de cura para transformar suas relações e viver o amor em sua plenitude."
-        videoSrc="https://www.youtube.com/embed/uk4wPrid9io?si=LKWNlEtAL1yZnQ7l"
-        buttonText="Baixe e transforme sua forma de amar!"
-        buttonLink=""
-      />
+      <Hero {...heroData} />
     </>
   );
 });
